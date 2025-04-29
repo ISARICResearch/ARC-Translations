76ce7abe951700481fc663d4c645f27d60ddeedf
saraduque <sara.duquevallejo@gmail.com>
2025-04-29 09:24:45 +0100

    french

--- code/code _modfied/translate_last_version.py
+++ code/code _modfied/translate_last_version.py
@@ -12,7 +12,7 @@ root_arch='C:/Users/sduquevallejo/Documents/GitHub/DataPlatform/ARCH/'
 
 root_arch_t='C:/Users/sduquevallejo/Documents/GitHub/ARC-Translations'
 
-most_recent_version_str='ARCH1.1.0-rc'
+most_recent_version_str='ARCH1.1.0'
 
 ###Translations parameters:
 #Language definitions to translate [('Language', 'Lang code')]
@@ -31,19 +31,19 @@ lists_file_path_src=root_arch+most_recent_version_str+'/Lists/'
 #Lists in the format[('list',['columns'])]. Columns list should be all the possible name of columns to be translated of each list file
 lists=[
 	('conditions',['Condition']), 
-	('demographics',['Region', 'Country', 'Disease']), 
+	('demographics',['Region', 'Country', 'Disease','Race']), 
 	('drugs', ['Drugs (Generic)', 'Drugs', 'IV fluids']), 
 	('inclusion', ['Disease']), 
-	('outcome', ['Outcome']), 
-	('pathogens', ['Microorganism']),
+	('outcome', ['Outcome','Disease']), 
+	('pathogens', ['Microorganism','Sample_Type']),
     ('followup', ['Outcome','Sequelae'])
 ]
 
 #Translation execution
 for lang in langs:
-	print("start lang: "+lang[0])
-	#for li in lists:
-	#	ListTranslation.translate_lists(f'{lists_file_path_src}/', li[0], li[1], arch_dir_path_des, lang)
+    print("start lang: "+lang[0])
+    for li in lists:
+        ListTranslation.translate_lists(f'{lists_file_path_src}/', li[0], li[1], arch_dir_path_des, lang)
 	#archtranslation.translate_arch(paper_file_path_src, paper_col_translate, arch_dir_path_des, lang)
-	archtranslation.translate_arch(arch_file_path_src, arch_col_translate, arch_dir_path_des, lang)
-	print("--------")
+    archtranslation.translate_arch(arch_file_path_src, arch_col_translate, arch_dir_path_des, lang)
+    print("--------")
